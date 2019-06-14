1920a6d97a7f65d28782a62f20d10287966b81fb
PKT92 <preston.tran@gmail.com>
2019-06-14 14:11:52 -0600

    Solved using brute force, may implement a more efficient code

--- Sudoku.py
+++ Sudoku.py
@@ -1,8 +1,56 @@
 import math
+import copy
+
+class Node:
+    def __init__(self, value):
+        self.dataval = value
+        self.sudokuVal = 1
+        self.nextNode = None
+        self.prevNode = None
+
+    def getValue(self):
+        return self.dataval
+    
+    def getNextNode(self):
+        return self.nextNode
+    
+    def getPrevNode(self):
+        return self.prevNode
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last = None
+        self.size = 0
+
+    def addNode(self, Node):
+        if self.head == None:
+            self.head = Node
+            self.last = Node
+        else:
+            self.last.nextNode = Node
+            Node.prevNode = self.last
+            self.last = Node
+        self.size += 1
+    
+    def printList(self):
+        node = self.head
+        while(node != None):
+            print(node.dataval)
+            node = node.nextNode
+    
+    def printListRev(self):
+        node = self.last
+        while(node != None):
+            print(node.dataval)
+            node = node.prevNode
+
+    def getHead(self):
+        return self.head
 
 def checkRow(n, row):
     if n in row:
-        print("Check row failed")
+        # print("Check row failed")
         return False
     else:
         return True
@@ -10,7 +58,7 @@ def checkRow(n, row):
 def checkCol(n, board, col):
     for i in board:
         if i[col] == n:
-            print("Check col failed")
+            # print("Check col failed")
             return False
     return True
 
@@ -20,7 +68,7 @@ def checkSquare(n, board, col, row):
     for i in range(rowMin, rowMin+3):
         for j in range(colMin, colMin+3):
             if board[i][j] == n:
-                print("Check square failed")
+                # print("Check square failed")
                 return False
     return True
 
@@ -35,16 +83,36 @@ def checkBoard(board):
                 board[i][j] = tmpVal
     return True
 
-path = 'test.txt'
+path = 'hard.txt'
 inputFile = open(path, 'r')
-test = [[] for i in range(9)]
+sudoku = [[] for i in range(9)]
+empties = LinkedList()
 for i in range(9):
     tmp = inputFile.readline().rstrip()
     for j in range(9):
-        test[i].append(tmp[j])
+        if tmp[j] == "_":
+            empties.addNode(Node([i,j]))
+        sudoku[i].append(tmp[j])
+solved = copy.deepcopy(sudoku)
+print(*sudoku, sep='\n')
 validBoard = True
-print(*test, sep='\n')
-if checkBoard(test):
-    print("Valid Board")
+currentNode = empties.getHead()
+while(currentNode != None):
+    row = currentNode.getValue()[0]
+    col = currentNode.getValue()[1]
+    tmpVal = str(currentNode.sudokuVal)
+    if checkCol(tmpVal, solved, col) and checkRow(tmpVal, solved[row]) and checkSquare(tmpVal, solved, col, row):
+        currentNode = currentNode.getNextNode()
+        solved[row][col] = tmpVal
+    else:
+        if currentNode.sudokuVal < 9:
+            currentNode.sudokuVal += 1
+        else:
+            currentNode.sudokuVal = 1
+            currentNode = currentNode.getPrevNode()
+            solved[row][col] = "_"
+print()
+if checkBoard(solved):
+    print(*solved, sep='\n')
 else:
     print("Invalid")
